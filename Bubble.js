0d8e84ab6ec92fa20d7433951c8a465d5b66065d
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-10-04 11:47:52 -0400

    bubble

--- Bubble.js
+++ Bubble.js
@@ -58,5 +58,20 @@ function swap(arr, index1, index2) {
 // }
 
 // *********************************************
+//********* REFACTOR BUBBLE SORT  ************
 
+function bubbleSort(arr) {
+	for (let i = arr.length; i > 0; i--) {
+		for (let j = 0; j < i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				let temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	return arr;
+}
+
+// *********************************************
 
